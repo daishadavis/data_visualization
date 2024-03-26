c4b6d7b360fd16f22e0c6e921833b49880bf635b
daishadavis <daishadavis84@gmail.com>
2024-03-26 19:39:50 -0400

    Inital commit

---
+++ cubes.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+#Data
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
+
+#Plot
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=10)
+
+# Set chart title and label axis.
+ax.set_title("Cube Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cube of Value")
+
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+#Set the range for each axis.
+ax.axis([0, 5000, 0, 125000000000])
+
+plt.savefig('cubes_plot.png', bbox_inches='tight')
