7ba9a40113724ba77b595bc83c4d01d614984db3
daishadavis <daishadavis84@gmail.com>
2024-03-27 21:22:41 -0400

    Creating random walks

--- cubes.py
+++ cubes.py
@@ -7,7 +7,7 @@ y_values = [x**3 for x in x_values]
 #Plot
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values,c=y_values, cmap=plt.cm.Reds, s=10)
 
 # Set chart title and label axis.
 ax.set_title("Cube Numbers", fontsize=24)
@@ -20,4 +20,4 @@ ax.tick_params(axis='both', which='major', labelsize=14)
 #Set the range for each axis.
 ax.axis([0, 5000, 0, 125000000000])
 
-plt.savefig('cubes_plot.png', bbox_inches='tight')
+plt.show()
